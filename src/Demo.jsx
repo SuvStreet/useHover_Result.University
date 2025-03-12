4e8ea9ac5eb69ea4f28d9266fba4a93776a9c34e
Dmitrij Kolotusha <suvstreet@gmail.com>
2025-03-12 21:11:06 +0300

    feat(useHover): Реализовать решение задачи.

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -6,7 +6,7 @@ export function Demo() {
   const { hovered, ref } = useHover()
 
   return (
-    <div ref={ref}>
+    <div ref={ref} className='hovered'>
       {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
     </div>
   )
